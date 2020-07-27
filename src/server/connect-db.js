970ff7e6f4e36aa92758471c878455f52630ab90
Arvind Kumar Singh <samratarvind114@gmail.com>
2020-07-27 15:48:01 +0530

    added mongoDB atlas

--- src/server/connect-db.js
+++ src/server/connect-db.js
@@ -1,5 +1,7 @@
 const { MongoClient } = require("mongodb");
-const url = process.env.MONGODB_URI || `mongodb://localhost:27017/organizer`;
+// const url = process.env.MONGODB_URI || `mongodb://localhost:27017/organizer`;
+const url =
+  "mongodb+srv://db_superuser:@Drexor1@to-do-cluster.cyvgt.mongodb.net/<dbname>?retryWrites=true&w=majority";
 let db = null;
 
 module.exports = {
